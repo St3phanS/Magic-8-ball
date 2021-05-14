749fc0eb0be8f00dad0c1ea90eaa409b7cccbd92
Stephan Squire <St3phanS@gmail.com>
2021-05-13 17:11:39 -0700

    The all seeing eye works, just doesnt have animations. So this is a save point before class

--- js/JScript.js
+++ js/JScript.js
@@ -1,12 +1,15 @@
 
-let answers = ["Ah I see it, yes","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again", "Don’t count on it","It is certain", "It is decidedly so","Most likely","My reply is no","My sources say no","Outlook not so good", "Outlook good", "Reply hazy, try again", "Signs point to yes", "Very doubtful", "Without a doubt", "Yes", "Yes – definitely", "You may rely on it"];
+
+let responses = ["Ah I see it, yes","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again", "Don’t count on it","It is certain", "It is decidedly so","Most likely","My reply is no","My sources say no","Outlook not so good", "Outlook good", "Reply hazy, try again", "Signs point to yes", "Very doubtful", "Without a doubt", "Yes", "Yes – definitely", "You may rely on it"];
 
 window.onload = function() {
     let eye = document.getElementById("eye");
-    let answer = document.getElementById("answer");
+    let response = document.getElementById("response");
     let iris = document.getElementById("iris");
     let question = document.getElementById("question");
+    let irisImg = document.getElementById('iris-img')
 
+  
     // function isNumeric(question){
     //     if (tyepof question ) {
             
@@ -14,19 +17,54 @@ window.onload = function() {
     // }
     question.addEventListener("keydown", function inputKeyUp(e) {
         if (e.key === 'Enter') {
-            // alert('success')
             
-            var format = /[?]/;
+            console.log(question.value.includes('?') )
 
-            if( question.match(format) ){
-                alert('question')
-              }else{
-                alert ('A question requires a question mark')
-              }
-            //var iChars = "?";
+            const questionLength = question.value.length;
+
+            if(questionLength<=1) {
+                alert('Enter a Question!');
+            }
+            else{
+
+                if(question.value.includes('?') === false){
+                    alert('A Question Requires a ?! \n Please Try Again')
+                }
+                else{
+    
+                    let whichResponse = Math.floor(Math.random()*responses.length);
+        
+
+                    $()
+                    
+                    irisImg.remove();
+                    response.innerText = responses[whichResponse];
+                    
+                    //response.innerText = responses[4]; //big text
+                    //response.innerText = responses[17]; //little text
+
+                    console.log(whichResponse)
+                }
+            }
+            
+            
+
+
+            
 
            
-            // submit
+            
         }
     });
 };
+
+
+// var questionSearch = question.search("?");
+//             console.log(questionSearch)
+
+//             if( questionSearch === -1 ){
+//                 alert('A question requires a question mark')
+//               }else{
+//                 alert ('question')
+//               }
+//             var iChars = "?";
